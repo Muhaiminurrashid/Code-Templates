afb562f329f5ecabf003c4ba20eeceb11570b43c
Muhaiminurrashid <muhaiminurrashid99@gmail.com>
2024-06-21 22:51:53 +0600

    first

---
+++ Competetive Template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define F first
+#define S second
+#define pb push_back;
+#define FL(i,a,b) for(int i=a;i<=b;i++)
+#define sz(x) x.size()
+#define ll long long
+using namespace std;
+
+void solve(){
+
+}
+int main(){
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+		int tt; cin>>tt;
+		while(tt--){
+			solve();
+		}
+		
+}
